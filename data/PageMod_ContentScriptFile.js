aa3aa51d7859d3deb6f8796205d2d6c73ae2c59f
Plamen Sharapanov <sharapanoff@hotmail.com>
2016-11-16 19:32:45 +0200

    *split the code into content-script and addon-script see https://developer.mozilla.org/en-US/Add-ons/SDK/High-Level_APIs/page-mod
    *content-script injects page-script via SCRIPT element

---
+++ data/PageMod_ContentScriptFile.js
@@ -0,0 +1,53 @@
+﻿self.port.on("Call_Page_Init", function ()
+{
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	script.text =
+
+"var _g = null;" +
+
+"window.addEventListener('signText-message-rs', function(event){" +
+"console.log(11);" +
+	"_g(event.detail);" +
+"}, false);"+
+
+"window.crypto = window.crypto || {};" +
+
+"window.crypto.signTextAsync = function(stringToSign, caOption){" +
+	"var event = document.createEvent('CustomEvent');" +
+	"event.initCustomEvent('signText-message-rq', true, true, { " +	
+		"'stringToSign': stringToSign,"+
+		"'caOption': caOption," +
+		"'misc':{" +
+			"'location': window.location.href," +
+			"'hostname': window.location.hostname," +
+			"'characterSet': window.document.characterSet," +
+		"}," +
+	"});" +
+	"document.documentElement.dispatchEvent(event);" +
+
+	"var p1 = new Promise(function(resolve, reject){_g=resolve;});"+
+	"return p1;" +
+"};";
+
+	document.body.appendChild(script);
+
+});
+
+
+//called from addon-script to content-script
+self.port.on("Call_Page_signTextRs", function (sgn)
+{
+	//call from content-script to page-script
+	var event = new CustomEvent("signText-message-rs", { bubbles: true, detail: sgn });
+	document.documentElement.dispatchEvent(event);
+});
+
+
+
+//called from page-script to content-script
+window.addEventListener('signText-message-rq', function (event)
+{
+	//call from content-script to addon-script
+	self.port.emit("Call_Addon_signTextRq", event.detail);
+}, false);
